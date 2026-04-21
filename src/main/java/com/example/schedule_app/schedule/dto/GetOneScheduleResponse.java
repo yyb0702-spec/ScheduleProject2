22cb1d234cbc79279bcbaaa4a75ddc42c26859bb
깡단 <yyb19920702@gmail.com>
2026-04-21 15:58:42 +0900

    feat: 댓글예외처리 파일추가 일정 조회시 댓글까지 볼수있도록 dto 추가

--- src/main/java/com/example/schedule_app/schedule/dto/GetOneScheduleResponse.java
+++ src/main/java/com/example/schedule_app/schedule/dto/GetOneScheduleResponse.java
@@ -1,8 +1,11 @@
 package com.example.schedule_app.schedule.dto;
 
+import com.example.schedule_app.comment.dto.GetCommentResponse;
+
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record GetOneScheduleResponse(Long id, String title, String content, String name, LocalDateTime createdAt,
-                                     LocalDateTime modifiedAt) {
+                                     LocalDateTime modifiedAt, List<GetCommentResponse> comments) {
 
 }
